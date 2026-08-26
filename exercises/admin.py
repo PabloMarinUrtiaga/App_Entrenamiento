9fb4647df00342e3c1de178927bd951be84b2f5f
Pablo <pablomarinurtiaga07@gmail.com>
2026-08-26 15:39:25 -0300

    Model: Exercises

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Exercise
 # Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_by", "created_at")
+    search_fields = ("name",)
+
+
+admin.site.register(Exercise, ExerciseAdmin)
--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="exercises/", null=True, blank=True)
+    youtube_url = models.URLField(blank=True)  # link que reenvía a un video de YouTube
+
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="created_exercises",
+        limit_choices_to={"role": "coach"},
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
