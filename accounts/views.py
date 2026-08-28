8d5b83716fd6bdb7e51d966518550d6828800bb8
Pablo <pablomarinurtiaga07@gmail.com>
2026-08-28 01:22:41 -0300

    add: View y urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from rest_framework import generics, permissions
+from .serializers import UserSerializer
+
+class MeView(generics.RetrieveUpdateAPIView):
+    # Devuelve/edita SOLO el perfil del usuario logueado (no una lista de todos)
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
