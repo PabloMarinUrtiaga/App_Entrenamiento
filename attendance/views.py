206fed788d839a382862105d5bc21e39022e984e
Pablo <pablomarinurtiaga07@gmail.com>
2026-08-28 14:43:09 -0300

    Add Attendance: views

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics, permissions
+from .models import Attendance
+from .serializers import AttendanceSerializer
+
+class MyAttendanceView(generics.ListCreateAPIView):
+    # El deportista ve y marca SU propia asistencia
+    serializer_class = AttendanceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Attendance.objects.filter(athlete=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(athlete=self.request.user, registered_by=self.request.user)
+
+
+class AttendanceListCreateView(generics.ListCreateAPIView):
+    # El entrenador ve/marca asistencia de cualquiera de sus deportistas
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(registered_by=self.request.user)
