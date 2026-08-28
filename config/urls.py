348091d5a401adaf06c2ef285974fd49235ccf81
Pablo <pablomarinurtiaga07@gmail.com>
2026-08-28 02:19:17 -0300

    add: exercises urls

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/accounts/", include("accounts.urls")),
     path("api/teams/", include("teams.urls")),
+     path("api/exercises/", include("exercises.urls")),
 ]
