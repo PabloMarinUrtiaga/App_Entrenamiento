25488c73ac04d9d3e7c4a5cd9a9b2903815451aa
Pablo <pablomarinurtiaga07@gmail.com>
2026-08-28 02:15:54 -0300

    fix: ReadOnlyFields solo created_by & created_At

--- exercises/serializers.py
+++ exercises/serializers.py
@@ -3,8 +3,7 @@ from .models import Exercise
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
-    # Usado por el Deportista: solo lectura de los ejercicios existentes
     class Meta:
         model = Exercise
         fields = ("id", "name", "description", "image", "youtube_url", "created_by", "created_at")
-        read_only_fields = fields
+        read_only_fields = ("created_by", "created_at")
